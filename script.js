d0e20174daa1ba05dc984db401b793d35ef3d447
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-02-11 11:49:09 +0530

    All filter of filter section is working fine

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let filter_2 = document.querySelector("#filter_2");
 let filter_3 = document.querySelector("#filter_3");
 let filter_4 = document.querySelector("#filter_4");
 
-let Filter_Edit_boxes_section = document.querySelector(".Filter_Edit_boxes_section");
+let Filter_Edit_boxes_section = document.querySelector(
+  ".Filter_Edit_boxes_section"
+);
 
 let basics_section = document.querySelector("#basics");
 let text_section = document.querySelector("#text");
@@ -13,21 +15,21 @@ let filter_section = document.querySelector("#filter");
 let choose_image_button = document.querySelector(".import_image_btn");
 let choose_input = document.querySelector(".import_image_btn input");
 let image_src = document.querySelector(".image_src img"); //Input image
-image_src.style.backgroundColor = "red"
 
+image_src.style.backgroundColor = "red";
 choose_image_button.addEventListener("click", () => choose_input.click());
 
 choose_input.addEventListener("change", () => {
-    let file = choose_input.files[0];
-    if(!file) return;
-    choose_image_button.style.display = "none";
-    image_src.src = URL.createObjectURL(file);
+  let file = choose_input.files[0];
+  if (!file) return;
+  choose_image_button.style.display = "none";
+  image_src.src = URL.createObjectURL(file);
 });
 
 // -----------------------------------------------
-// zoom in / zoom out 
-let zoom_input = document.getElementById('zoomRange')
-zoom_input.addEventListener('input', function() {
+// zoom in / zoom out
+let zoom_input = document.getElementById("zoomRange");
+zoom_input.addEventListener("input", function () {
   const scale = this.value;
   // const zoomImage = document.getElementById('zoomImage');
   image_src.style.transform = `scale(${scale})`;
@@ -44,11 +46,21 @@ let saturate_button = document.querySelector("#filter .box:nth-child(4)");
 let invert_button = document.querySelector("#filter .box:nth-child(5)");
 let blur_button = document.querySelector("#filter .box:nth-child(6)");
 
-let brightness_edit_box = document.querySelector(".Filter_Edit_boxes_section .edit_box:nth-child(1)");
-let contrast_edit_box = document.querySelector(".Filter_Edit_boxes_section .edit_box:nth-child(2)");
-let saturate_edit_box = document.querySelector(".Filter_Edit_boxes_section .edit_box:nth-child(3)");
-let invert_edit_box = document.querySelector(".Filter_Edit_boxes_section .edit_box:nth-child(4)");
-let blur_edit_box = document.querySelector(".Filter_Edit_boxes_section .edit_box:nth-child(5)");
+let brightness_edit_box = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(1)"
+);
+let contrast_edit_box = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(2)"
+);
+let saturate_edit_box = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(3)"
+);
+let invert_edit_box = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(4)"
+);
+let blur_edit_box = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(5)"
+);
 
 brightness_button.addEventListener("click", () => {
   brightness_button.style.backgroundColor = "#01c7f81a";
@@ -61,7 +73,7 @@ brightness_button.addEventListener("click", () => {
   saturate_edit_box.style.display = "none";
   invert_edit_box.style.display = "none";
   blur_edit_box.style.display = "none";
-})
+});
 contrast_button.addEventListener("click", () => {
   contrast_button.style.backgroundColor = "#01c7f81a";
   brightness_button.style.backgroundColor = "transparent";
@@ -73,7 +85,7 @@ contrast_button.addEventListener("click", () => {
   saturate_edit_box.style.display = "none";
   invert_edit_box.style.display = "none";
   blur_edit_box.style.display = "none";
-})
+});
 saturate_button.addEventListener("click", () => {
   saturate_button.style.backgroundColor = "#01c7f81a";
   brightness_button.style.backgroundColor = "transparent";
@@ -85,43 +97,92 @@ saturate_button.addEventListener("click", () => {
   saturate_edit_box.style.display = "flex";
   invert_edit_box.style.display = "none";
   blur_edit_box.style.display = "none";
-})
+});
 invert_button.addEventListener("click", () => {
   invert_button.style.backgroundColor = "#01c7f81a";
   brightness_button.style.backgroundColor = "transparent";
   contrast_button.style.backgroundColor = "transparent";
   saturate_button.style.backgroundColor = "transparent";
-  blur_button.style.backgroundColor = "transparent";  
+  blur_button.style.backgroundColor = "transparent";
   brightness_edit_box.style.display = "none";
   contrast_edit_box.style.display = "none";
   saturate_edit_box.style.display = "none";
   invert_edit_box.style.display = "flex";
   blur_edit_box.style.display = "none";
-})
+});
 blur_button.addEventListener("click", () => {
   blur_button.style.backgroundColor = "#01c7f81a";
   brightness_button.style.backgroundColor = "transparent";
   contrast_button.style.backgroundColor = "transparent";
   saturate_button.style.backgroundColor = "transparent";
-  invert_button.style.backgroundColor = "transparent";  
+  invert_button.style.backgroundColor = "transparent";
   brightness_edit_box.style.display = "none";
   contrast_edit_box.style.display = "none";
   saturate_edit_box.style.display = "none";
   invert_edit_box.style.display = "none";
   blur_edit_box.style.display = "flex";
-})
+});
 
-let brightness_range_input = document.querySelector(".Filter_Edit_boxes_section .edit_box:nth-child(1) input");
-brightness_range_input.addEventListener("input", function() {
-  let brightness_value = this.value;
-  image_src.style.filter = `brightness(${brightness_value}%)`;
+// All Filter section input range:
+let brightness_range_input = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(1) input"
+);
+let contrast_range_input = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(2) input"
+);
+let saturate_range_input = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(3) input"
+);
+let invert_range_input = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(4) input"
+);
+let blur_range_input = document.querySelector(
+  ".Filter_Edit_boxes_section .edit_box:nth-child(5) input"
+);
+
+// default value of filters:
+let brightness_value = 100; // 100 is default value | Range 0 - 200
+let contrast_value = 100; // 100 is default value | Range 0 - 200
+let saturate_value = 100; // 100 is default value | Range 0 - 200
+let invert_value = 0; // 0 is default value | Range 0 - 100
+let blur_value = 0; // 0 is default value | Range 0 - 100
+
+// Brightness input range :
+brightness_range_input.addEventListener("input", function () {
+  brightness_value = this.value;
   document.querySelector("#brightnss_value").innerHTML = `${brightness_value}%`;
-  // imgSrc.style.filter = `brightness(${brightness}%) contrast(${contrast}%) saturate(${saturate}%) invert(${invert}%) blur(${blur}px)`
+  image_src.style.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+});
 
-})
+// Contrast input range :
+contrast_range_input.addEventListener("input", function () {
+  contrast_value = this.value;
+  document.querySelector("#contrast_value").innerHTML = `${contrast_value}%`;
+  image_src.style.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+});
+
+// Saturate input range :
+saturate_range_input.addEventListener("input", function () {
+  saturate_value = this.value;
+  document.querySelector("#saturate_value").innerHTML = `${saturate_value}%`;
+  image_src.style.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+});
+
+// Invert input range :
+invert_range_input.addEventListener("input", function () {
+  invert_value = this.value;
+  document.querySelector("#invert_value").innerHTML = `${invert_value}%`;
+  image_src.style.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+});
+
+// Blur input range :
+blur_range_input.addEventListener("input", function () {
+  blur_value = this.value;
+  document.querySelector("#blur_value").innerHTML = `${blur_value}%`;
+  image_src.style.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+});
 
 // ___________________________________________________
-
 
 filter_1.style.backgroundColor = "red";
 filter_1.addEventListener("click", () => {
@@ -161,4 +222,47 @@ filter_4.addEventListener("click", () => {
   text_section.style.display = "none";
   drawing_section.style.display = "none";
   Filter_Edit_boxes_section.style.display = "block";
+});
+
+// Reset and Save button:
+let reset_button = document.querySelector(
+  ".reset_and_save_btn > button:nth-child(1)"
+);
+let save_button = document.querySelector(
+  ".reset_and_save_btn > button:nth-child(2)"
+);
+
+// If user click reset button:
+reset_button.addEventListener("click", () => {
+  if (confirm("Do you want really want to reset")) {
+    brightness_value = 100;
+    contrast_value = 100;
+    saturate_value = 100;
+    invert_value = 0;
+    blur_value = 0;
+    image_src.style.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+  }
+});
+
+save_button.addEventListener("click", function () {
+  if (confirm("Do you want download image")) {
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  canvas.width = image_src.naturalWidth;
+  canvas.height = image_src.naturalHeight;
+  ctx.filter = `brightness(${brightness_value}%) contrast(${contrast_value}%) saturate(${saturate_value}%) invert(${invert_value}%) blur(${blur_value}px)`;
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  // ctx.scale(flip_x, flip_y);
+  ctx.drawImage(
+    image_src,
+    -canvas.width / 2,
+    -canvas.height / 2,
+    canvas.width,
+    canvas.height
+  );
+  const link = document.createElement("a");
+  link.download = "image.jpg";
+  link.href = canvas.toDataURL();
+  link.click();
+}
 });
